586abac1737041c233b43f38fadb4a72e33e7f64
Aurelien Morvan <aurelien.morvan@smartpush.fr>
2022-08-05 23:48:32 +0200

    Init project

---
+++ api/src/Infrastructure/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MyBudget;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
